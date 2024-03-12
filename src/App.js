c1e7b0fc3b9baedbce9f67de37686b26b3260660
ranaadeel15911 <ra0511083@gmail.com>
2024-03-12 07:47:21 +0500

    afj

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+function App() {
+  return (
+    <>
+     <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    
+    </>
+  );
+}
+
+export default App;
